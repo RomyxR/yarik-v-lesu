7537e242fe995f3474fcc5996eaa6dea43f40eea
Romyx <157620445+RomyxR@users.noreply.github.com>
2026-03-24 16:25:44 +0300

    Update update.py

--- update.py
+++ update.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 import requests
+from concurrent.futures import ThreadPoolExecutor
 
 black_urls = [
     "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/BLACK_VLESS_RUS.txt",
     "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/BLACK_VLESS_RUS_mobile.txt",
     "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/BLACK_SS%2BAll_RUS.txt",
+    "https://raw.githubusercontent.com/ebrasha/free-v2ray-public-list/refs/heads/main/V2Ray-Config-By-EbraSha.txt",
 ]
 
 white_urls = [
@@ -15,23 +17,22 @@ white_urls = [
     "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/Vless-Reality-White-Lists-Rus-Mobile-2.txt",
 ]
 
-def preprocess_vpn_list(urls):
-    raw_vpn_list = []
-    for url in urls:
-        r = requests.get(url.strip(), timeout=10)
-        r.raise_for_status()
-        vpn_urls = [line for line in r.text.split("\n") if not line.startswith("#") and line.strip()]
-        raw_vpn_list.extend(vpn_urls)
-    vpn_list = list(set(raw_vpn_list))
-    print(str(len(vpn_list)) + "/" + str(len(raw_vpn_list)))
-    return vpn_list
+def fetch(url):
+    try:
+        return [l for l in requests.get(url, timeout=10).text.splitlines() if l and not l.startswith("#")]
+    except: return []
 
-print("Processing black list...")
+def preprocess_vpn_list(urls):
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        results = list(executor.map(fetch, urls))
+    
+    raw_list = [item for sublist in results for item in sublist]
+    return sorted(set(raw_list))
+
+
 with open("black_list.txt", "w", encoding="utf-8") as f:
     f.write("\n".join(sorted(preprocess_vpn_list(black_urls))))
 
-print("Processing white list...")
 with open("white_list.txt", "w", encoding="utf-8") as f:
     f.write("\n".join(sorted(preprocess_vpn_list(white_urls))))
 
-print("Done!")
