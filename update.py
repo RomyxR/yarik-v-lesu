78d3295ac2e090d442cae685823f4bb287b828de
Romyx <157620445+RomyxR@users.noreply.github.com>
2026-03-24 21:32:16 +0300

    Update update.py

--- update.py
+++ update.py
@@ -1,7 +1,10 @@
 import requests
 from concurrent.futures import ThreadPoolExecutor
+import geoip2.database
 import socket
-import ssl
+import re
+
+DB_PATH = 'GeoLite2-Country.mmdb'
 
 black_urls = [
     "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/BLACK_VLESS_RUS.txt",
@@ -18,54 +21,40 @@ white_urls = [
     "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/Vless-Reality-White-Lists-Rus-Mobile-2.txt",
 ]
 
-def is_alive(line):
-    try:
-        if "://" in line:
-            part = line.split('@')[1].split('#')[0]
-            host_port, params_str = part.split('?')[0], part.split('?')[1] if '?' in part else ''
-            host, port = host_port.rsplit(':', 1)
-            is_tls = "security=tls" in params_str or "security=reality" in params_str or "publicKey=" in params_str
-            sni = host
-            if "sni=" in params_str: sni = params_str.split('sni=')[1].split('&')[0]
-        else:
-            if ':' not in line: return True
-            host, port = line.split('/')[0].rsplit(':', 1)
-            is_tls, sni = False, None
+def get_country(host: str):
+    if not host.replace('.', '').isdigit():
+        try:
+            host = socket.gethostbyname(host)
+        except Exception:
+            return "Unknown"
 
-        s = socket.socket(); s.settimeout(2)
-        s.connect((host, int(port)))
-        
-        if is_tls:
-            ctx = ssl.create_default_context()
-            ctx.check_hostname, ctx.verify_mode = False, ssl.CERT_NONE
-            s = ctx.wrap_socket(s, server_hostname=sni)
-            s.close()
-            return True
-        else:
-            s.close()
-            return True
-    except: return False
+    with geoip2.database.Reader(DB_PATH) as reader:
+        response = reader.country(host)
+    return response.country.name or "Unknown"
 
-def fetch(url):
-    try:
-        return [l for l in requests.get(url, timeout=10).text.splitlines() if l and not l.startswith("#")]
+def write_as_country(line: str):
+    pattern = r'(.*?://[^@]+@)([^#]+)(#)(.+)'
+    match = re.match(pattern, line)
+    
+    if not match: return line
+    
+    raw_link = list(match.groups())
+    raw_link[3] = get_country(raw_link[1].split(":")[0])
+    return "".join(raw_link).replace('?#', '#')
+
+def fetch(url: str):
+    try: return [l for l in requests.get(url, timeout=10).text.splitlines() if l and not l.startswith("#")]
     except: return []
 
-def preprocess_vpn_list(urls):
+def preprocess_vpn_list(urls: list):
     with ThreadPoolExecutor(max_workers=10) as executor:
         results = list(executor.map(fetch, urls))
     raw_list = [item for sublist in results for item in sublist]
     return sorted(set(raw_list))
-
-def get_alive_vpn(vpn_list: list):
-    with ThreadPoolExecutor(max_workers=100) as executor:
-        results = list(executor.map(is_alive, vpn_list))
-    alive_list = [line for line, is_ok in zip(vpn_list, results) if is_ok]
-    return sorted(set(alive_list))
    
 
 with open("black_list.txt", "w", encoding="utf-8") as f:
-    f.write("\n".join(sorted(get_alive_vpn(preprocess_vpn_list(black_urls)))))
+    f.write("\n".join(sorted(map(write_as_country, preprocess_vpn_list(black_urls)))))
 
 with open("white_list.txt", "w", encoding="utf-8") as f:
-    f.write("\n".join(sorted(get_alive_vpn(preprocess_vpn_list(white_urls)))))
+    f.write("\n".join(sorted(preprocess_vpn_list(white_urls))))
